3a5c2679ed1527270427a127e68f516439517991
Ben Townsend <benctownsend98@gmail.com>
2023-01-17 15:15:43 -0500

    some more pages

---
+++ test.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Testing Javascript on my webpage</h2>
+
+<p> To make sure you read my webpage, I'm going to ask you some questions about me...</p>
+
+<p id="Q1">Where do I go to school?</p>
+
+<button type="button" onclick='document.getElementById("Q1").innerHTML = "Penn State"'>Answer</button>
+
+</body>
+</html>
